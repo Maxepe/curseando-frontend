00d0fbc5f530b370d4c25bd06b3c536d9b80b78d
maxipet <maxipetrei@outlook.com>
2025-12-05 10:24:04 -0300

    prod environment config

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,7 +2,7 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api/v1',  // TODO: Update with production API URL
+  apiUrl: 'http://curseando-backend-prod.eba-ttmuudmn.us-east-1.elasticbeanstalk.com/api/v1',
   locale: 'es-AR',
   toast: {
     timeout: 4000
